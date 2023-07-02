53881d141ce32679571b0349fb66e93edd5cc4a9
Roshantechnology <115568270+Roshantechnology@users.noreply.github.com>
2023-07-02 23:06:50 +0530

    Add files via upload

---
+++ CheckNumber.java
@@ -0,0 +1,16 @@
+package JavaMockTest_1;
+
+public class CheckNumber {
+
+	public static void main(String[] args) {
+		int a=-5;
+		if(a<0)
+			System.out.println("Given Number is Negative ::"+a);
+		else if(a>0)
+			System.out.println("Given Number is Positive ::"+a);
+		else
+			System.out.println("Given Number is Zero ::"+a);
+
+	}
+
+}
